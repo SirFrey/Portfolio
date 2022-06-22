5f3137b756a5de507d0273bf1109faaf74d4202f
Moises Castellanos <moisescastellano12312@gmail.com>
2022-06-22 00:09:32 -0400

    Header casi terminado

--- src/script.js
+++ src/script.js
@@ -1,14 +1,20 @@
 const bar = document.querySelector('.bar');
 const x = document.querySelector('.x');
 const principal_nav = document.querySelector('.principal_nav');
+const div = document.querySelector('.body_secondary');
+const container_bar = document.querySelector('.container_bar');
+
+
 bar.addEventListener('click',()=>{ 
     principal_nav.classList.toggle('moved_nav');
+    div.classList.toggle('shadow');
+    container_bar.classList.toggle('shadow');
 });
 
 x.addEventListener('click',()=>{ 
     principal_nav.classList.toggle('moved_nav');
+    div.classList.toggle('shadow');
+    container_bar.classList.toggle('shadow')
 })
-
-
 
 
