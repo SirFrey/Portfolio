e70cb04eb25605deca2cde09b33bad5c7eb50da2
SirFrey <moisescastellano12312@gmail.com>
2022-07-19 23:45:14 -0400

    Header base finished

--- src/script.js
+++ src/script.js
@@ -1,20 +1,35 @@
-const bar = document.querySelector('.bar');
-const x = document.querySelector('.x');
-const principal_nav = document.querySelector('.principal_nav');
-const div = document.querySelector('.body_secondary');
-const container_bar = document.querySelector('.container_bar');
+'use strict';
 
+const dom = document;
+const bar = dom.querySelector('.bar');
+const x = dom.querySelector('.x');
+const principal_nav = dom.querySelector('.principal_nav');
+const div = dom.querySelector('.body_secondary');
+const background_bar = dom.querySelector('.background');
 
-bar.addEventListener('click',()=>{ 
-    principal_nav.classList.toggle('moved_nav');
-    div.classList.toggle('shadow');
-    container_bar.classList.toggle('shadow');
+bar.addEventListener('click',()=>{
+    principal_nav.classList.replace('no_active','active') 
+    principal_nav.style.right = '0';
+    div.style.filter = 'blur(2px)';
+    background_bar.style.filter = 'blur(2px)';
 });
 
-x.addEventListener('click',()=>{ 
-    principal_nav.classList.toggle('moved_nav');
-    div.classList.toggle('shadow');
-    container_bar.classList.toggle('shadow')
+
+    // document.body.addEventListener('click', ()=>{
+    //     if(principal_nav.classList.contains('active')){
+    //         principal_nav.classList.replace('active','no_active') 
+    //         principal_nav.style.right = '-300px';
+    //         div.style.filter = 'none';
+    //         background_bar.style.filter = 'none';
+    //     }
+    // })
+
+
+x.addEventListener('click',()=>{
+    principal_nav.classList.replace('active','no_active') 
+    principal_nav.style.right = '-300px';
+    div.style.filter = 'none';
+    background_bar.style.filter = 'none';
 })
 
 
