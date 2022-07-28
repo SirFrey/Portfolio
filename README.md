3cb2b64fa6fd81d4a71d426e8f2ba0d3bd825100
SirFrey <moisescastellano12312@gmail.com>
2022-07-28 17:31:53 -0400

    bugs fixed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MiPortafolio
+
 Este es uno de mis primeros proyectos que es mi portafolio, este contendra todos los proyectos que haya hecho y también mostrar las herramientas utiles que se utilizar en la rama de la programación
--- src/script.js
+++ src/script.js
@@ -7,29 +7,16 @@ const principal_nav = dom.querySelector('.principal_nav');
 const div = dom.querySelector('.body_secondary');
 const background_bar = dom.querySelector('.background');
 
-bar.addEventListener('click',()=>{
-    principal_nav.classList.replace('no_active','active') 
-    principal_nav.style.right = '0';
-    div.style.filter = 'blur(2px)';
-    background_bar.style.filter = 'blur(2px)';
+bar.addEventListener('click', () => {
+	principal_nav.classList.replace('no_active', 'active');
+	principal_nav.style.right = '0';
+	div.style.filter = 'blur(2px)';
+	background_bar.style.filter = 'blur(2px)';
 });
 
-
-    // document.body.addEventListener('click', ()=>{
-    //     if(principal_nav.classList.contains('active')){
-    //         principal_nav.classList.replace('active','no_active') 
-    //         principal_nav.style.right = '-300px';
-    //         div.style.filter = 'none';
-    //         background_bar.style.filter = 'none';
-    //     }
-    // })
-
-
-x.addEventListener('click',()=>{
-    principal_nav.classList.replace('active','no_active') 
-    principal_nav.style.right = '-300px';
-    div.style.filter = 'none';
-    background_bar.style.filter = 'none';
-})
-
-
+x.addEventListener('click', (e) => {
+	principal_nav.classList.replace('active', 'no_active');
+	principal_nav.style.right = '-300px';
+	div.style.filter = 'none';
+	background_bar.style.filter = 'none';
+});
