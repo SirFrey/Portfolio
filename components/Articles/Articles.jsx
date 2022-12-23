87e154ca0cb81769163ac35112df9ce027f0991b
SirFrey <moisescastellano12312@gmail.com>
2022-12-23 12:50:25 -0400

    mend

--- components/Articles/Articles.jsx
+++ components/Articles/Articles.jsx
@@ -67,6 +67,12 @@ const Articles = () => {
 						src='https://img.freepik.com/foto-gratis/diseno-concepto-plantilla-diseno-web-html_53876-120438.jpg?w=900&t=st=1660676885~exp=1660677485~hmac=2899e1383827e6af6608d048fbba3d6958763202894ec02e3e5bb4d598f6e3b5'
 						className={s.bgFixed}
 					/>
+					<div className={s.container}>
+						<div className={s.chevron}></div>
+						<div className={s.chevron}></div>
+						<div className={s.chevron}></div>
+						<span className={s.text}>Scroll down</span>
+					</div>
 				</section>
 				<section>
 					<div className='marginContainer'>
