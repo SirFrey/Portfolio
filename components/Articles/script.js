c5cf6fce0fc4189485d4254e46e3132887238617
SirFrey <moisescastellano12312@gmail.com>
2023-01-05 19:22:38 -0400

    bugs fixes

--- components/Articles/script.js
+++ components/Articles/script.js
@@ -25,11 +25,11 @@ export function textOpacityEffectDomino() {
 							counter = 0;
 							textSvgLi[index].style.opacity = 1;
 							textSvgLi[index].style.filter = 'blur(0)';
-							return;
+							return null;
 						} else {
 							textSvgLi[index].style.opacity = 0;
 							textSvgLi[index].style.filter = 'blur(4px)';
-							return;
+							return null;
 						}
 					});
 					counter++;
