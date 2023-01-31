a51d221b4274673e61c7e1a9efda8dd5ce29833c
SirFrey <moisescastellano12312@gmail.com>
2023-01-31 17:54:41 -0400

    website more responsive

--- components/SliderCard/SliderCard.jsx
+++ components/SliderCard/SliderCard.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from 'react';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import s from './SliderCard.module.css';
 import Slider from 'react-slick/lib/slider';
+import { Octokit } from 'octokit';
+
+const octokit = new Octokit({
+	auth: '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',
+});
 
 const SliderCard = () => {
+	const [repos, setRepos] = useState();
+	const fetchRepos = async () => {
+		const res = await octokit.request('GET /users/sirfrey/repos', {});
+		console.log(res);
+	};
+	useEffect(() => {
+		fetchRepos();
+	});
 	const settings = {
 		dots: true,
-		// infinite: true,
 		speed: 500,
 		slidesToShow: 1,
 		slidesToScroll: 1,
