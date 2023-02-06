a5fa9c5b07788b41c972e56d064ac33e7856c430
SirFrey <moisescastellano12312@gmail.com>
2023-02-06 15:34:45 -0400

    Minor inconsistencies fixes and added new style of arrows on the slider

--- components/SliderCard/SliderCard.jsx
+++ components/SliderCard/SliderCard.jsx
@@ -1,15 +1,52 @@
-import { useEffect } from 'react';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import s from './SliderCard.module.css';
 import Slider from 'react-slick/lib/slider';
+// Arrow button within the slider
 
-// const octokit = new Octokit({
-// 	auth: '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',
-// });
+const NextArrow = ({ onClick }) => {
+	return (
+		<button className={`${s.arrow} ${s.left}`} onClick={onClick}>
+			<svg width='30px' height='80px' viewBox='0 0 50 80' xmlSpace='preserve'>
+				<polyline
+					fill='none'
+					stroke='#FFFFFF'
+					strokeWidth='1'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					points='
+	45.63,75.8 0.375,38.087 45.63,0.375 '
+				/>
+			</svg>
+		</button>
+	);
+};
+const PrevArrow = ({ onClick }) => {
+	return (
+		<button className={`${s.arrow} ${s.right}`} onClick={onClick}>
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				xmlnsXlink='http://www.w3.org/1999/xlink'
+				width='30px'
+				height='80px'
+				viewBox='0 0 50 80'
+				xmlSpace='preserve'
+			>
+				<polyline
+					fill='none'
+					stroke='#FFFFFF'
+					strokeWidth='1'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					points='
+	0.375,0.375 45.63,38.087 0.375,75.8 '
+				/>
+			</svg>
+		</button>
+	);
+};
 
 const SliderCard = () => {
-	useEffect(() => {});
 	const settings = {
 		dots: true,
 		speed: 500,
@@ -18,6 +55,8 @@ const SliderCard = () => {
 		adaptiveHeight: true,
 		autoplay: true,
 		className: s.slider,
+		nextArrow: <NextArrow />,
+		prevArrow: <PrevArrow />,
 	};
 	return (
 		<>
