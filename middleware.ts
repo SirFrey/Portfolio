5452b326ccf9d0fb4ca75f74bc488481a0d86803
Moises <moisescastellano12312@gmail.com>
2024-08-21 21:06:29 -0400

    style(card): improve card design package(framer): updated

--- middleware.ts
+++ middleware.ts
@@ -15,6 +15,12 @@ function getLocale(request: NextRequest) {
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
+  // check all routes to print hello world in the console
+  if (pathname.match('/*')) {
+    console.log('hello world')
+  }
+
+
   const pathnameHasLocale = locales.some(
     locale => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   )
