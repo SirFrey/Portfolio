de4f326c307e862478ee66556a2bf02541b78055
Moises Castellanos <moisescastellano12312@gmail.com>
2026-04-13 22:32:01 -0400

    fix(redirect): avoid default redirect page

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     defaultLocale: 'es',
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: true,
+      redirectToDefaultLocale: false,
     },
   },
   image: {
